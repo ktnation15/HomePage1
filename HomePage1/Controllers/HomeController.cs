829e07ec813b35a8a93fab65eefc92a08a6beacf
John Mayer <John Mayer@DESKTOP-1HRM93L>
2024-09-05 14:47:25 +0200

    Add project files.

---
+++ HomePage1/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+using HomePage1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HomePage1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Cv()
+        {
+            return View();
+        }
+        public IActionResult AboutMe()
+        {
+            return View();
+        }
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
